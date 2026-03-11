1b18b0e22fcc9a4e0a62e3fe141212da824194a9
Shaxriyor <shaxa2654@gmail.com>
2026-03-11 07:32:45 +0500

    .

--- src/Address.java
+++ src/Address.java
@@ -4,4 +4,11 @@ public class Address {
     private String state;
     private String zipCode;
     private String country;
+    public Address(String streetAddress, String city, String state, String zipCode, String country) {
+        this.streetAddress = streetAddress;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.country = country;
+    }
 }
