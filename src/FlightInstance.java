36c5ed44b48c803ff66446abb046de4bc0338d2f
Shaxriyor <shaxa2654@gmail.com>
2026-03-11 13:41:03 +0500

    .

--- src/FlightInstance.java
+++ src/FlightInstance.java
@@ -5,17 +5,23 @@ public class FlightInstance {
     private String gate;
     private FlightStatus status;
     private Aircraft aircraft;
+    private List<FlightReservation> reservations = new ArrayList<>();
     public FlightInstance(Date departureTime, String gate, Aircraft aircraft) {
         this.departureTime = departureTime;
         this.gate = gate;
         this.status = FlightStatus.SCHEDULED;
         this.aircraft=aircraft;
     }
-    public boolean cancel(){
-        this.status = FlightStatus.CANCELLED;
-        return true;
-    }
     public void updateStatus(FlightStatus status){
         this.status = status;
+    }
+    public boolean cancel() {
+        this.status = FlightStatus.CANCELLED;
+        for(FlightReservation res : reservations) {
+            res.setStatus(ReservationStatus.CANCELLED);
+            Notification alert = new EmailNotification(101, new Date, "URGENT: Your flight has been cancelled.", "passenger@email.com");
+            alert.send();
+        }
+        return true;
     }
 }
